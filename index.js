f689d0496fcff1cf3b7dfffed070e4da4ab5ca89
karlstomberg <karlstom@udel.edu>
2019-09-06 14:12:16 -0400

    Made things switch

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
+console.log("index.js loaded");
+
 $(function(){
+
+    console.log("index.js jquery loaded");
     
     $('#nameform').on("submit", function( event ){
 
-        console.log("Boo");
-
         event.preventDefault();
 
-        $('.jumbotron').innerHTML = $('lname').innerHTML + " " + $('fname').innerHTML;
-        $('.jumbotron').show();
+        $('.jumbotron').html("<h1>" + $('#lname').val() + " " + $('#fname').val() + "</h1>");
+        $('.jumbotron').removeClass("d-none");
 
         return false;
 
